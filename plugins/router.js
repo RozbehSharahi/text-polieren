a659b51c564dc8e90d474d68ec9d31b57052461f
Rozbeh Chiryai Sharahi <rozbeh.sharahi@turbinekreuzberg.com>
2021-01-24 20:06:36 +0000

    Initial background fade

--- plugins/router.js
+++ plugins/router.js
@@ -1,13 +1,14 @@
 import eventService from '~/services/event-service'
 import Page from '~/models/Page'
+import timeout from '~/utils/timeout'
 
 export default async function ({ app }) {
   const pages = (await app.$content('pages').fetch()).map((v) => new Page(v))
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  app.router.beforeEach((to, from, next) => {
+  app.router.afterEach(async (to) => {
+    await timeout(50)
     const newPage = pages.find((v) => v.getPath() === to.fullPath)
     eventService.$emit('background-change', newPage.getBackgroundImage())
-    next()
   })
 }
